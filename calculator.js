4398d8e8fb4f07c05e66514af5832cd0ec08fe34
thesoundfromthesky <38703562+thesoundfromthesky@users.noreply.github.com>
2018-12-11 17:39:55 -0600

    Layout updates
    basic style

---
+++ calculator.js
@@ -0,0 +1,111 @@
+function getSelector(_selector)
+{
+    let selector = _selector;
+    let element = document.querySelector(selector);
+    return element;
+}
+
+function setInput(_self){
+    let self = _self;
+    let input = getSelector("#input");
+    if (_self.value !== undefined)
+        self = _self.value;
+    let temp = input.value;
+    temp += self;
+    if (isNaN(temp))
+        return;        
+    input.value += self;
+}
+
+function clearInput(){
+    let result = getSelector("#result");
+    let input = getSelector("#input");
+    result.value = null;
+    input.value = null;
+}
+
+function calculate(_operator){
+    let operator = _operator;
+    let result = getSelector("#result");
+    let input = getSelector("#input");
+
+    if (Number.isNaN(Number(input.value)))
+        return;
+
+    debugger;
+    let inputPointIndex = input.value.indexOf('.');
+    let inputDigit = 0;
+    if(-1 < inputPointIndex)
+    {
+        let inputLen = input.value.length;
+        inputDigit = inputLen - inputPointIndex - 1;
+    }
+
+    let resultPointIndex = result.value.indexOf('.');
+    let resultDigit = 0;
+    if(-1 < resultPointIndex)
+    {
+        let resultLen = result.value.length;
+        resultDigit = resultLen - resultPointIndex - 1;
+    }
+    
+    let digit = resultDigit;
+    if(resultDigit < inputDigit)
+        digit = inputDigit;
+    let decimal = 10 ** digit;
+    if (-1 < inputPointIndex || -1 < resultPointIndex) {
+        result.value = (result.value * decimal).toFixed();
+        input.value = (input.value * decimal).toFixed();
+    }
+
+    let resultNum = Number.parseInt(result.value);
+    let inputNum = Number.parseInt(input.value);
+
+    if (Number.isNaN(resultNum))
+        resultNum = 0;
+
+    if (_operator.value !== undefined)
+        operator = _operator.value;
+
+    switch (operator) {
+        case '+':
+            resultNum += inputNum;
+            break;
+        case '-':
+            resultNum -= inputNum;
+            break;
+        case '*':
+            resultNum *= inputNum;
+            break;
+        case '/':
+            resultNum /= inputNum;
+            break;
+        default:
+            return;
+    }
+    if (-1 < inputPointIndex || -1 < resultPointIndex)
+        result.value = resultNum / decimal;
+    else
+        result.value = resultNum;
+    input.value = null;
+}
+
+document.body.addEventListener("keydown", (event) => {
+    let element = document.activeElement;
+    if (element.id !== "input") {
+        if (event.key === '+' || event.key === '-' ||
+            event.key === '*' || event.key === '/')
+            this.calculate(event.key);
+        else if ('0' <= event.key && event.key <= '9')
+            this.setInput(event.key);
+        else if (event.key === '.')
+            this.setInput(event.key);
+        else if (event.key === 'Delete') {
+            let input = getSelector("#input");
+            let len = input.value.length;
+            if(0 < len)
+                input.value = input.value.substring(0, len - 1);
+        }
+    }
+
+})
---
+++ myCanvas.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function getElement(selector) {
+  return document.querySelector(selector);
+}
+
+function getCanvas2D() {
+  let c = document.querySelector("#myCanvas");
+  return c.getContext("2d");
+}
+
+function drawLine(x, y, width, height) {
+  let ctx = getCanvas2D();
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + width, y + height);
+  ctx.stroke();
+}
+
+function drawCircle(x, y, r) {
+  let ctx = getCanvas2D();
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+function fillText(text, x, y) {
+  let ctx = getCanvas2D();
+  ctx.font = "30px Arial";
+  ctx.fillText(text, x, y);
+}
+
+function drawText(text, x, y) {
+  let ctx = getCanvas2D();
+  ctx.font = "30px Arial";
+  ctx.strokeText(text, x, y);
+}
+
+drawLine(250, 50, 100, 0);
+drawCircle(200, 200, 20);
+fillText("Hello World", 0, 30);
+drawText("Hello World", 0, 60);
